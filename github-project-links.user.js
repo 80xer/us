ce78aab50dd156da319656a0759264434c62fa69
de.lee <80xer.s@gmail.com>
2019-04-04 17:12:36 +0900

    change class for github basic mode and dark mode

--- github-project-links.user.js
+++ github-project-links.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Github project links
+// @name        Github project links 2
 // @version     1.0
 // @description A userscript that adds a menu that shows a list of all the project links in the main menu.
 // @author      80xer
@@ -25,17 +25,13 @@
     .prjsMenu { position: relative; }
     .prjsMenu:hover div.prjsWrap { display: block; }
     a.prjsLink { position: absolute; top: -10px; z-index: 2; }
-    div.prjsWrap { position: absolute; width: 280px; z-index: 2; top: 8px; left: -10px; background: transparent; padding: 10px;  display:none;  }
+    div.prjsWrap { position: absolute; width: 280px; z-index: 2; top: 8px; left: -10px; padding: 10px; display: none; }
     .tmpbefore { border-bottom-color: #343434; border: 8px solid transparent; }
-    ul.prjs:before { position: absolute; left: 24px; top: -16px; content: ''; display: inline-block; border: 8px solid transparent; border-bottom-color: #343434; }
-    ul.prjs { position:relative; list-style: none; background-color: #181818; border: 1px solid #404040; border: 1px solid #404040; box-shadow: 0 1px 15px #000; border-radius: 3px; }
+    ul.prjs:before { position: absolute; left: 34px; top: -16px; content: ''; display: inline-block; }
+    ul.prjs { position:relative; list-style: none; width: auto; }
     .tmpafter {  content: ''; }
-    ul.prjs:after { position: absolute; left: 25px; top: -14px; content: ''; display: inline-block; border: 7px solid transparent; border-bottom-color: #181818; }
-    ul.prjs li { border-bottom: 1px solid #343434; }
-    ul.prjs li:hover { background: #4183C4; }
-    ul.prjs li:last-child { border-bottom: none; }
+    ul.prjs:after { position: absolute; left: 35px; top: -14px; content: ''; display: inline-block; margin-left: -9px; }
     ul.prjs li a { display: flex; padding: 10px 12px; text-decoration: none;}
-    ul.prjs li a:hover { color: #bebebe !important; }
   `);
 
   function $(str, el) {
@@ -92,18 +88,27 @@
   function setProjectList(projects) {
     const prjsWrap = document.createElement('div');
     addClass(prjsWrap, 'prjsWrap');
-    addClass(prjsWrap, 'Box');
     const prjs = document.createElement('ul');
     addClass(prjs, 'prjs');
+    addClass(prjs, 'Box');
+    addClass(prjs, 'Popover-message');
+    addClass(prjs, 'jump-to-suggestions');
+    addClass(prjs, 'jump-to-suggestions-results-container');
 
     projects.forEach(project => {
       const prj = document.createElement('li');
+      addClass(prj, 'navigation-item');
       const anchor = document.createElement('a');
-      addClass(anchor, 'link-gray-dark');
-      addClass(anchor, 'mr-1');
+      addClass(anchor, 'jump-to-suggestions-path');
       anchor.setAttribute('href', project.href);
       anchor.textContent = project.name;
       prj.appendChild(anchor);
+      prj.onmouseenter = function(e) {
+        e.target.setAttribute('aria-selected', true);
+      };
+      prj.onmouseleave = function(e) {
+        e.target.setAttribute('aria-selected', false);
+      };
       prjs.appendChild(prj);
     });
 
