d9791604f2eb4bd291ac84a8a8d8e0f4f66ce754
de.lee <80xer.s@gmail.com>
2019-04-05 14:36:36 +0900

    up version

--- github-project-links.user.js
+++ github-project-links.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Github project links
-// @version     1.2.1
+// @version     1.3.1
 // @description A userscript that adds a menu that shows a list of all the project links in the main menu.
 // @author      80xer
 // @namespace   https://github.com/80xer
