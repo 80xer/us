ef224d241b5c6ebc2eacd4ea184d195c5376fe16
de.lee <80xer.s@gmail.com>
2019-04-05 12:03:44 +0900

    깃헙 구조 변경

--- github-project-links.user.js
+++ github-project-links.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        Github project links
-// @version     1.0.2
+// @version     1.1
 // @description A userscript that adds a menu that shows a list of all the project links in the main menu.
 // @author      80xer
 // @namespace   https://github.com/80xer
 // @match       https://github.com/*
+// @run-at      document-end
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
@@ -16,16 +17,17 @@
 
 (function() {
   'use strict';
+  const user = $('.Header-item .avatar');
+  if (!user) {
+    GM_log('not found user avatar');
+    return;
+  }
 
-  const userNav = $('#user-links');
-  if (!userNav) return;
-  // ul.header-search-wrapper
-  // li.jump-to-suggestions-path
   GM_addStyle(`
     .prjsMenu { position: relative; }
     .prjsMenu:hover div.prjsWrap { display: block; }
-    a.prjsLink { position: absolute; top: -10px; z-index: 2; }
-    div.prjsWrap { position: absolute; width: 280px; z-index: 2; top: 8px; left: -10px; padding: 10px; display: none; }
+    a.prjsLink { position: absolute; z-index: 3; }
+    div.prjsWrap { position: absolute; width: 280px; z-index: 2; top: 20px; left: -10px; padding: 10px; display: none; }
     .tmpbefore { border-bottom-color: #343434; border: 8px solid transparent; }
     ul.prjs:before { position: absolute; left: 34px; top: -16px; content: ''; display: inline-block; }
     ul.prjs { position:relative; list-style: none; width: auto; }
@@ -56,87 +58,107 @@
     else el.className += ' ' + className;
   }
 
-  // create projects menu
-  const menus = $('nav>ul');
-  const menu = $('li:last-child', menus).cloneNode(true);
-  addClass(menu, 'prjsMenu');
+  function createMenu() {
+    return new Promise((resolve, reject) => {
+      const menus = $$('nav a');
+      const explore = menus.filter(e => e.textContent.trim() === 'Explore')[0];
 
-  const prjsLink = $('a', menu);
-  prjsLink.textContent = 'Projects';
-  prjsLink.setAttribute('href', '/orgs/lawcompany/projects');
-  addClass(prjsLink, 'prjsLink');
-
-  // open projects
-  const openState = $('.table-list-header-toggle.states > a:first-child');
-  const projects = $$('#projects-results > div');
-
-  // set list in storage
-  if (hasClass(openState, 'selected') && projects.length > 0) {
-    const projectArray = projects.map(project => {
-      const info = project.children[1];
-      const anchor = $('h4 a', info);
-      let bar;
-      if (info.children.length >= 3) {
-        bar = info.children[2].cloneNode(true).outerHTML;
+      if (!explore || explore.length < 1) {
+        reject({ msg: 'not found explore menu' });
       }
-      return {
-        href: anchor.getAttribute('href'),
-        name: anchor.textContent,
-        bar: bar,
-      };
+
+      const menu = document.createElement('div');
+      addClass(menu, 'prjsMenu');
+
+      const prjsLink = explore.cloneNode(true);
+      prjsLink.textContent = 'Projects';
+      prjsLink.setAttribute('href', '/orgs/lawcompany/projects');
+      addClass(prjsLink, 'prjsLink');
+      menu.appendChild(prjsLink);
+      explore.parentNode.appendChild(menu);
+      resolve(menu);
     });
-    GM_setValue('prjList', projectArray);
   }
 
-  function setProjectList(projects) {
-    const prjsWrap = document.createElement('div');
-    addClass(prjsWrap, 'prjsWrap');
-    const prjs = document.createElement('ul');
-    addClass(prjs, 'prjs');
-    addClass(prjs, 'Box');
-    addClass(prjs, 'Popover-message');
-    addClass(prjs, 'jump-to-suggestions');
-    addClass(prjs, 'jump-to-suggestions-results-container');
+  function getProjectListToStorage() {
+    // open projects
+    const openState = $('.table-list-header-toggle.states > a:first-child');
+    const projects = $$('#projects-results > div');
+    // set list in storage
+    if (hasClass(openState, 'selected') && projects.length > 0) {
+      const projectArray = projects.map(project => {
+        const info = project.children[1];
+        const anchor = $('h4 a', info);
+        let bar;
+        if (info.children.length >= 3) {
+          bar = info.children[2].cloneNode(true).outerHTML;
+        }
+        return {
+          href: anchor.getAttribute('href'),
+          name: anchor.textContent,
+          bar: bar,
+        };
+      });
+      GM_setValue('prjList', projectArray);
+    }
+    return GM_getValue('prjList');
+  }
 
-    projects.forEach(project => {
-      const prj = document.createElement('li');
-      addClass(prj, 'navigation-item');
-      const anchor = document.createElement('a');
-      addClass(anchor, 'jump-to-suggestions-path');
-      anchor.setAttribute('href', project.href);
-      const name = document.createElement('div');
-      name.textContent = project.name;
-      anchor.appendChild(name);
-      if (project.bar) {
-        const bar = document.createElement(null);
-        anchor.appendChild(bar);
-        bar.outerHTML = project.bar;
+  function createProjectList(menu, projects) {
+    return new Promise((resolve, reject) => {
+      if (projects || projects.length <= 0) {
+        resolve();
       }
-      prj.appendChild(anchor);
-      prj.onmouseenter = function(e) {
-        e.target.setAttribute('aria-selected', true);
-      };
-      prj.onmouseleave = function(e) {
-        e.target.setAttribute('aria-selected', false);
-      };
-      prjs.appendChild(prj);
+
+      const prjsWrap = document.createElement('div');
+      addClass(prjsWrap, 'prjsWrap');
+      const prjs = document.createElement('ul');
+      addClass(prjs, 'prjs');
+      addClass(prjs, 'Box');
+      addClass(prjs, 'Popover-message');
+      addClass(prjs, 'jump-to-suggestions');
+      addClass(prjs, 'jump-to-suggestions-results-container');
+
+      projects.forEach(project => {
+        const prj = document.createElement('li');
+        addClass(prj, 'navigation-item');
+        const anchor = document.createElement('a');
+        addClass(anchor, 'jump-to-suggestions-path');
+        anchor.setAttribute('href', project.href);
+        const name = document.createElement('div');
+        name.textContent = project.name;
+        anchor.appendChild(name);
+        if (project.bar) {
+          const bar = document.createElement(null);
+          anchor.appendChild(bar);
+          bar.outerHTML = project.bar;
+        }
+        prj.appendChild(anchor);
+        prj.onmouseenter = function(e) {
+          e.target.setAttribute('aria-selected', true);
+        };
+        prj.onmouseleave = function(e) {
+          e.target.setAttribute('aria-selected', false);
+        };
+        prjs.appendChild(prj);
+      });
+
+      prjsWrap.appendChild(prjs);
+      menu.appendChild(prjsWrap);
+      resolve();
     });
-
-    prjsWrap.appendChild(prjs);
-    menu.appendChild(prjsWrap);
   }
 
-  // get list in storage
-  const prjList = GM_getValue('prjList');
-
-  if (prjList && prjList.length > 0) {
-    setProjectList(prjList);
-    /*
-    prjsLink.addEventListener('click', (e) => {
-        e.preventDefault();
-    }, false);
-    */
-  }
-
-  menus.appendChild(menu);
+  createMenu()
+    .then(menu => {
+      const prjList = getProjectListToStorage();
+      return createProjectList(menu, prjList);
+    })
+    .then(() => {
+      GM_log(`>>> OK`);
+    })
+    .catch(e => {
+      GM_log(e);
+      GM_log(`>>> ERR : ${e.msg}`);
+    });
 })();
